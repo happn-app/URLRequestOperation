7b919fbb1484e764b775fb613ec77914a107bb9e
Frizlab <fload@me.com>
2021-11-02 19:55:08 +0100

    Clarify the non-asynchronicity of the validate method of the response validator

--- Sources/URLRequestOperation/Operations/Helper Protocols/URLResponseValidator.swift
+++ Sources/URLRequestOperation/Operations/Helper Protocols/URLResponseValidator.swift
@@ -14,9 +14,14 @@ public protocol URLResponseValidator {
 	 
 	 - Note: The closest API to this one in the data task session delegate is asynchronous
 	 (method is `urlSession(:,dataTask:,didReceive:,completionHandler:)`).
-	 I do not know why the method is asynchronous.
-	 Considering this and the fact we cannot validate responses properly in an asynchronous manner for download tasks,
-	 we’ve decided to make this API synchronous. */
+	 
+	 The asynchronicity allows the dev to let the user choose whether he allows downloading a file, if
+	 the response indicates the data task should be transformed to a download task for instance
+	 ([see this Developer Forums post](https://developer.apple.com/forums/thread/693645?answerId=693559022#693559022)).
+	 
+	 URLRequestOperation’s goal is not to replace URLSession’s delegate.
+	 If you need to convert your task to a download task, or need to be async in general when validating the response,
+	 you should use the session’s delegate. */
 	func validate(urlResponse: URLResponse) -> Error?
 	
 }
