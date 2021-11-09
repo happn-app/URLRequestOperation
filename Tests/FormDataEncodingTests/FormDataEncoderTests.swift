e70a18d5d31e554111f45a3b30217362fa069ea2
Frizlab <fload@me.com>
2021-11-09 21:41:23 +0100

    Implement more than basic test for form data encoder

--- Tests/FormDataEncodingTests/FormDataEncoderTests.swift
+++ Tests/FormDataEncodingTests/FormDataEncoderTests.swift
@@ -5,9 +5,32 @@ import XCTest
 
 
 
+class Bob : Equatable, Encodable, Decodable, MultipartPartConvertible {
+	
+	static func == (lhs: Bob, rhs: Bob) -> Bool {
+		return true
+	}
+	
+	var multipart: MultipartPart? {
+		return MultipartPart(headers: HTTPHeaders(dictionaryLiteral: ("yo", "magueule")), body: "sup")
+	}
+	
+	init() {
+	}
+	
+	required init?(multipart: MultipartPart) {
+	}
+	
+}
+
+
 class FormDataEncoderTests : XCTestCase {
 	
-	func testBasicEncode() {
+	func testBasicEncode() throws {
+		let toEncode = ["Hello": Bob()]
+		let encoded = try FormDataEncoder().encode(toEncode, boundary: "123")
+		let decoded = try FormDataDecoder().decode([String: Bob].self, from: encoded, boundary: "123")
+		XCTAssertEqual(toEncode, decoded)
 	}
 	
 }
