313e0126c0a7359d6e51f76e24f6abc5b57f1ddc
Frizlab <fload@me.com>
2021-11-27 11:55:04 +0100

    Create the MediaType module

---
+++ Sources/MediaType/MediaType.swift
@@ -0,0 +1,200 @@
+/*
+Copyright 2021 happn
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+import Foundation
+
+
+
+public struct MediaType : Hashable, RawRepresentable {
+	
+	public typealias RawValue = String
+	
+	public struct Parameter : Hashable {
+		
+		var key: String
+		var value: String
+		
+		public func hash(into hasher: inout Hasher) {
+			hasher.combine(key.lowercased())
+			hasher.combine(value)
+		}
+		
+		public static func ==(_ lhs: Parameter, _ rhs: Parameter) -> Bool {
+			return lhs.key.lowercased() == rhs.key.lowercased() && lhs.value == rhs.value
+		}
+		
+	}
+	
+	public var type: String
+	public var subtype: String
+	
+	public var parameters: [Parameter]
+	
+	/** We do not allow obs-text chars in parameter (%x80-FF) */
+	public init?(rawValue: String) {
+		/* Syntax (from https://datatracker.ietf.org/doc/html/rfc7231#section-3.1.1.1):
+		 * media-type = type "/" subtype *( OWS ";" OWS parameter )
+		 * type       = token
+		 * subtype    = token
+		 * parameter  = token "=" ( token / quoted-string )
+		 *
+		 * From https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.6 we have:
+		 * token          = 1*tchar
+		 * tchar          = "!" / "#" / "$" / "%" / "&" / "'" / "*"
+		 *                / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+		 *                / DIGIT / ALPHA
+		 *                ; any VCHAR, except delimiters
+		 * quoted-string  = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+		 * qdtext         = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+		 * obs-text       = %x80-FF
+		 * quoted-pair    = "\" ( HTAB / SP / VCHAR / obs-text )
+		 */
+		var currentScannedString: NSString? /* We still need this as we have deploy target lower than macOS 10.15/iOS 13, etc. */
+		let scanner = Scanner(string: rawValue)
+		scanner.charactersToBeSkipped = CharacterSet() /* We skip nothing */
+		
+		guard scanner.scanCharacters(from: .tchar, into: &currentScannedString) else {return nil}
+		assert(currentScannedString!.length > 0)
+		self.type = currentScannedString! as String
+		
+		guard scanner.scanString("/", into: nil) else {return nil}
+		
+		guard scanner.scanCharacters(from: .tchar, into: &currentScannedString) else {return nil}
+		assert(currentScannedString!.length > 0)
+		self.subtype = currentScannedString! as String
+		
+		var parameters = [Parameter]()
+		while !scanner.isAtEnd {
+			_ = scanner.scanCharacters(from: .ws, into: nil)
+			guard scanner.scanString(";", into: nil) else {return nil}
+			_ = scanner.scanCharacters(from: .ws, into: nil)
+			
+			guard scanner.scanCharacters(from: .tchar, into: &currentScannedString) else {return nil}
+			assert(currentScannedString!.length > 0)
+			let key = currentScannedString! as String
+			
+			guard scanner.scanString("=", into: nil) else {return nil}
+			
+			let value: String
+			if scanner.scanString("\"", into: nil) {
+				/* We must parse a quoted string */
+				guard let v = Self.parseQuotedString(from: scanner) else {return nil}
+				value = v
+			} else {
+				guard scanner.scanCharacters(from: .tchar, into: &currentScannedString) else {return nil}
+				assert(currentScannedString!.length > 0)
+				value = currentScannedString! as String
+			}
+			
+			parameters.append(Parameter(key: key, value: value))
+		}
+		assert(scanner.isAtEnd)
+		
+		self.parameters = parameters
+	}
+	
+	public var rawValue: String {
+#warning("Quoted string for parameter value if cannot be represented raw")
+		return type.lowercased() + "/" + subtype.lowercased() + parameters.reduce("", { $0 + ";" + $1.key.lowercased() + "=" + $1.value.quotedIfNeeded() })
+	}
+	
+	public subscript(_ parameterKey: String) -> String? {
+		return parameter(forKey: parameterKey)
+	}
+	
+	public func parameter(forKey parameterKey: String) -> String? {
+		return parameters.map{ (key: $0.key, value: $0.value) }.last{ $0.key.lowercased() == parameterKey.lowercased() }?.value
+	}
+	
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(type.lowercased())
+		hasher.combine(subtype.lowercased())
+		hasher.combine(parameters)
+	}
+	
+	public static func ==(_ lhs: MediaType, _ rhs: MediaType) -> Bool {
+		return lhs.type.lowercased() == rhs.type.lowercased() && lhs.subtype.lowercased() == rhs.subtype.lowercased() && lhs.parameters == rhs.parameters
+	}
+	
+	/* Copy-pasted from https://github.com/Frizlab/LinkHeaderParser/blob/f4c241cf9609dec9bc8727671669e5c99a0a094e/Sources/LinkHeaderParser/LinkHeaderParser.swift */
+	private static func parseQuotedString(from scanner: Scanner, currentlyParsed: String = "") -> String? {
+		var parsedString = currentlyParsed
+		var currentScannedString: NSString?
+		
+		/* Let’s try and parse whatever legal characters we can from the quoted string.
+		 * The backslash and double-quote chars are not in the set we parse here, so the scanner will stop at these (among other). */
+		if scanner.scanCharacters(from: .qdtext, into: &currentScannedString) {
+			parsedString += currentScannedString! as String
+		}
+		
+		/* Now let’s see if we stopped at a backlash.
+		 * If so, we’ll retrieve the next char, verify it is in the legal charset for a backslashed char,
+		 * add it to the parsed string, and continue parsing the quoted string from there. */
+		guard !scanner.scanString("\\", into: nil) else {
+			guard !scanner.isAtEnd else {return nil}
+			
+			/* Whatever char we have at the current location will be added to the parsed string (if in quotedPairSecondCharCharacterSet).
+			 * We have to do ObjC-index to Swift index conversion though… */
+			
+			guard let swiftIdx = Range(NSRange(location: scanner.scanLocation, length: 0), in: scanner.string)?.lowerBound else {return nil}
+			let addedStr = String(scanner.string[swiftIdx])
+			scanner.scanLocation += 1
+			
+			guard addedStr.rangeOfCharacter(from: .quotedPairSecondChar) != nil else {return nil}
+			parsedString += addedStr
+			
+			return parseQuotedString(from: scanner, currentlyParsed: parsedString)
+		}
+		
+		/* We have now consumed all legal chars from a quoted string and are not stopped on a backslash.
+		 * The only legal char left is a double quote!
+		 * Which also signals the end of the quoted string. */
+		guard scanner.scanString("\"", into: nil) else {return nil}
+		return parsedString
+	}
+	
+}
+
+
+internal extension String {
+	
+	func quotedIfNeeded() -> String {
+		return self
+	}
+	
+}
+
+
+internal extension CharacterSet {
+	
+	static let sp = CharacterSet(charactersIn: " ")
+	static let htab = CharacterSet(charactersIn: "\t")
+	
+	static let ws = sp.union(htab)
+	
+	static let digit = CharacterSet(charactersIn: "0123456789")
+	static let alpha = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	static let tchar = CharacterSet(charactersIn: "!#$%&'*+-.^_`|~").union(digit).union(alpha)
+	
+	static let qdtext = ws
+		.union(CharacterSet(arrayLiteral: Unicode.Scalar(0x21)))
+		.union(CharacterSet(charactersIn: Unicode.Scalar(0x23)...Unicode.Scalar(0x5b)))
+		.union(CharacterSet(charactersIn: Unicode.Scalar(0x5d)...Unicode.Scalar(0x7e)))
+		/* no obs-text */
+	static let quotedPairSecondChar = ws
+		.union(CharacterSet(charactersIn: Unicode.Scalar(0x21)...Unicode.Scalar(0x7e)))
+		/* no obs-text */
+	
+}
---
+++ Tests/MediaTypeTests/MediaTypeTests.swift
@@ -0,0 +1,31 @@
+import Foundation
+import XCTest
+
+@testable import MediaType
+
+
+
+class MediaTypeTests : XCTestCase {
+	
+	func testRFCExample() throws {
+		let ex1 = try XCTUnwrap(MediaType(rawValue: #"text/html;charset=utf-8"#))
+		let ex2 = try XCTUnwrap(MediaType(rawValue: #"text/html;charset=UTF-8"#))
+		let ex3 = try XCTUnwrap(MediaType(rawValue: #"Text/HTML;Charset="utf-8""#))
+		let ex4 = try XCTUnwrap(MediaType(rawValue: #"text/html; charset="utf-8""#))
+		
+		XCTAssertEqual(ex1, ex3)
+		/* We do not compare ex1 and ex2 though doc says it should be equal, because we do not support parameter semantics. */
+		XCTAssertEqual(ex1, ex4)
+		XCTAssertEqual(ex3, ex4) /* If both previous one are true, this one better be true! */
+		
+		let normalized = "text/html;charset=utf-8"
+		XCTAssertEqual(ex1.rawValue, normalized)
+		XCTAssertEqual(ex2.rawValue, "text/html;charset=UTF-8") /* Same as above, we do not support parameter semantics. */
+		XCTAssertEqual(ex3.rawValue, normalized)
+		XCTAssertEqual(ex4.rawValue, normalized)
+		
+		XCTAssertEqual(ex1.type,    "text")
+		XCTAssertEqual(ex1.subtype, "html")
+	}
+	
+}
