081bac202d414e7ec1a849889a47ab4e0c29ec8d
Frizlab <fload@me.com>
2021-11-28 01:22:46 +0100

    Implement image init

--- Sources/URLRequestOperation/Operations/Helpers/Retry Providers/UnretriedErrorsRetryProvider.swift
+++ Sources/URLRequestOperation/Operations/Helpers/Retry Providers/UnretriedErrorsRetryProvider.swift
@@ -6,6 +6,24 @@ import RetryingOperation
 
 public struct UnretriedErrorsRetryProvider : RetryProvider {
 	
+	public static func forStatusCodes(_ codes: Set<Int> = Set(400..<500)) -> UnretriedErrorsRetryProvider {
+		return Self{ err in
+			switch err as? Err {
+				case .unexpectedStatusCode(let v, httpBody: nil)?: return v.flatMap{ codes.contains($0) } ?? true
+				default:                                           return false
+			}
+		}
+	}
+	
+	public static func forImageConversion() -> UnretriedErrorsRetryProvider {
+		return Self{ err in
+			switch err as? Err {
+				case .cannotConvertToImage?: return true
+				default:                     return false
+			}
+		}
+	}
+	
 	public let isBlacklistedError: (Error) -> Bool
 	
 	public init(isBlacklistedError: @escaping (Error) -> Bool) {
