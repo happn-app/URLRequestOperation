9d762b006ada6c0e830f71ba7ab781c82bb013d5
Frizlab <fload@me.com>
2021-12-09 17:13:30 +0100

    Add path parameter to image init

--- Sources/URLRequestOperation/Operations/URLRequestDataOperation+Images.swift
+++ Sources/URLRequestOperation/Operations/URLRequestDataOperation+Images.swift
@@ -43,10 +43,11 @@ public extension URLRequestDataOperation {
 	}
 	
 	static func forImage(
-		url: URL, headers: [String: String?] = [:], cachePolicy: NSURLRequest.CachePolicy = .useProtocolCachePolicy, session: URLSession = .shared,
+		baseURL: URL, path: String? = nil, headers: [String: String?] = [:], cachePolicy: NSURLRequest.CachePolicy = .useProtocolCachePolicy, session: URLSession = .shared,
 		resultProcessingDispatcher: BlockDispatcher = SyncBlockDispatcher(),
 		requestProcessors: [RequestProcessor] = [], retryProviders: [RetryProvider] = [NetworkErrorRetryProvider()]
 	) -> URLRequestDataOperation<ResultType> where ResultType == Image {
+		let url = path.flatMap{ baseURL.appendingPathComponent($0) } ?? baseURL
 		var request = URLRequest(url: url, cachePolicy: cachePolicy)
 		for (key, val) in headers {request.setValue(val, forHTTPHeaderField: key)}
 		return Self.forImage(
