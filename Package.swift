f3f7474dd73a9f54b3e453fb74698e9b73f2adee
Frizlab <fload@me.com>
2022-12-17 21:25:57 +0100

    Enhance the Package.swift file

--- Package.swift
+++ Package.swift
@@ -7,36 +7,35 @@ let swiftSettings: [SwiftSetting] = []
 
 let package = Package(
 	name: "URLRequestOperation",
-	products: {
-		var ret = [Product]()
-		ret.append(.library(name: "MediaType", targets: ["MediaType"]))
-		ret.append(.library(name: "URLRequestOperation", targets: ["URLRequestOperation"]))
-		return ret
-	}(),
-	dependencies: {
-		var ret = [Package.Dependency]()
-		ret.append(.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"))
-		ret.append(.package(url: "https://github.com/happn-app/HTTPCoders.git", from: "0.1.0"))
-		ret.append(.package(url: "https://github.com/happn-app/RetryingOperation.git", from: "1.1.6"))
-		ret.append(.package(url: "https://github.com/happn-app/SemiSingleton.git", from: "2.1.0-beta.1"))
-		return ret
-	}(),
-	targets: {
-		var ret = [Target]()
-		ret.append(.target(name: "MediaType", swiftSettings: swiftSettings))
-		ret.append(.testTarget(name: "MediaTypeTests", dependencies: ["MediaType"], swiftSettings: swiftSettings))
+	products: buildArray{
+		$0.append(.library(name: "MediaType", targets: ["MediaType"]))
+		$0.append(.library(name: "URLRequestOperation", targets: ["URLRequestOperation"]))
+	},
+	dependencies: buildArray{
+		$0.append(.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"))
+		$0.append(.package(url: "https://github.com/happn-app/HTTPCoders.git", from: "0.1.0"))
+		$0.append(.package(url: "https://github.com/happn-app/RetryingOperation.git", from: "1.1.6"))
+		$0.append(.package(url: "https://github.com/happn-app/SemiSingleton.git", from: "2.1.0-beta.1"))
+	},
+	targets: buildArray{
+		$0.append(.target(name: "MediaType", swiftSettings: swiftSettings))
+		$0.append(.testTarget(name: "MediaTypeTests", dependencies: ["MediaType"], swiftSettings: swiftSettings))
 		
-		ret.append(.target(name: "URLRequestOperation", dependencies: {
-			var ret = [Target.Dependency]()
-			ret.append(.product(name: "FormURLEncodedCoder", package: "HTTPCoders"))
-			ret.append(.product(name: "Logging",             package: "swift-log"))
-			ret.append(.product(name: "RetryingOperation",   package: "RetryingOperation"))
-			ret.append(.product(name: "SemiSingleton",       package: "SemiSingleton"))
-			ret.append(.target(name: "MediaType"))
-			return ret
-		}(), swiftSettings: swiftSettings))
-		ret.append(.testTarget(name: "URLRequestOperationTests", dependencies: ["URLRequestOperation"], swiftSettings: swiftSettings))
-		ret.append(.executableTarget(name: "URLRequestOperationManualTest", dependencies: ["URLRequestOperation"], swiftSettings: swiftSettings))
-		return ret
-	}()
+		$0.append(.target(name: "URLRequestOperation", dependencies: buildArray{
+			$0.append(.product(name: "FormURLEncodedCoder", package: "HTTPCoders"))
+			$0.append(.product(name: "Logging",             package: "swift-log"))
+			$0.append(.product(name: "RetryingOperation",   package: "RetryingOperation"))
+			$0.append(.product(name: "SemiSingleton",       package: "SemiSingleton"))
+			$0.append(.target(name: "MediaType"))
+		}, swiftSettings: swiftSettings))
+		$0.append(.testTarget(name: "URLRequestOperationTests", dependencies: ["URLRequestOperation"], swiftSettings: swiftSettings))
+		$0.append(.executableTarget(name: "URLRequestOperationManualTest", dependencies: ["URLRequestOperation"], swiftSettings: swiftSettings))
+	}
 )
+
+
+func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
+	var ret = [Element]()
+	builder(&ret)
+	return ret
+}
