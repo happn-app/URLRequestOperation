93b7e2ad9ed46fec4e9924daba9ca4ec87885e92
Frizlab <fload@me.com>
2022-12-23 16:49:23 +0200

    Set to log requests and responses in manual test

--- Sources/URLRequestOperationManualTest/main.swift
+++ Sources/URLRequestOperationManualTest/main.swift
@@ -46,6 +46,9 @@ class SessionDelegate : NSObject, URLSessionTaskDelegate, URLSessionDataDelegate
 
 /* ************************************************************ */
 
+URLRequestOperationConfig.logHTTPRequests = true
+URLRequestOperationConfig.logHTTPResponses = true
+
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 let session = URLSession(configuration: .ephemeral, delegate: URLRequestOperationSessionDelegateProxy(SessionDelegate(id: 1)), delegateQueue: nil)
 #else
